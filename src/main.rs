6644a2104ea873f7969b5cf93f8424be22d0c965
Alistair A. Israel <aisrael@gmail.com>
2020-07-27 17:21:51 +0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,68 @@
+use clap::Clap;
+use std::collections::HashMap;
+
+/// This doc string acts as a help message when the user runs '--help'
+/// as do all doc strings on fields
+#[derive(Clap)]
+#[clap(version = "1.0", author = "Alistair Israel <aisrael@gmail.com>")]
+struct Opts {
+    /// Sets a custom config file. Could have been an Option<T> with no default too
+    #[clap(short, long, default_value = "default.conf")]
+    config: String,
+    /// Some input. Because this isn't an Option<T> it's required to be used
+    input: String,
+    /// A level of verbosity, and can be used multiple times
+    #[clap(short, long, parse(from_occurrences))]
+    verbose: i32,
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Clap)]
+enum SubCommand {
+    #[clap(version = "1.3", author = "Someone E. <someone_else@other.com>")]
+    Test(Test),
+}
+
+/// A subcommand for controlling testing
+#[derive(Clap)]
+struct Test {
+    /// Print debug info
+    #[clap(short)]
+    debug: bool,
+}
+
+fn main() {
+    let opts: Opts = Opts::parse();
+
+    // Gets a value for config if supplied by user, or defaults to "default.conf"
+    println!("Value for config: {}", opts.config);
+    println!("Using input file: {}", opts.input);
+
+    // Vary the output based on how many times the user used the "verbose" flag
+    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
+    match opts.verbose {
+        0 => println!("No verbose info"),
+        1 => println!("Some verbose info"),
+        2 => println!("Tons of verbose info"),
+        3 | _ => println!("Don't be crazy"),
+    }
+
+    // You can handle information about subcommands by requesting their matches by name
+    // (as below), requesting just the name used, or both at the same time
+    match opts.subcmd {
+        SubCommand::Test(t) => {
+            if t.debug {
+                println!("Printing debug info...");
+            } else {
+                println!("Printing normally...");
+            }
+        }
+    }
+
+    let resp = reqwest::blocking::get("https://httpbin.org/ip")
+        .unwrap()
+        .json::<HashMap<String, String>>()
+        .unwrap();
+    println!("{:#?}", resp);
+}
